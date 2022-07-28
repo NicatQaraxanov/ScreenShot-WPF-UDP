81460fe88f8be77c7f685cb4c2ae2e78ef05997f
nijatgarakhanov <96003352+nijatgarakhanov@users.noreply.github.com>
2022-07-28 22:08:32 +0400

    ScreenShot using UDP (WPF & Network programming)

---
+++ SS/ClientServer/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread t;
+
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            t = new Thread(() =>
+            {
+                using var listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+
+                var ip = IPAddress.Parse("127.0.0.1");
+                var port = 27121;
+
+                try
+                {
+                    EndPoint listenerEP = new IPEndPoint(ip, port);
+                    listener.Bind(listenerEP);
+
+
+                    var buffer = new byte[ushort.MaxValue - 30];
+
+                    EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+                    while (true)
+                    {
+                        listener.ReceiveFrom(buffer, ref remoteEP);
+
+                        Dispatcher.Invoke(() => 
+                        { 
+                        Img.Source = LoadImage(buffer);
+                        });
+                    }
+                }
+                catch
+                {
+                    listener.Close();
+                }
+            });
+            t.Start();
+        }
+
+        protected virtual void OnExit(System.Windows.ExitEventArgs e)
+        {
+            t.Abort();
+        }
+
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+
+            using var client = new Socket(
+                                     AddressFamily.InterNetwork,
+                                     SocketType.Dgram,
+                                     ProtocolType.Udp);
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            var port = 54678;
+
+            EndPoint remoteEP = new IPEndPoint(ip, port);
+
+            byte[] buffer = null;
+
+            buffer = Encoding.Default.GetBytes("SS");
+            client.SendTo(buffer, remoteEP);
+            client.Close();
+        }
+
+
+
+        private static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+    }
+}
---
+++ SS/Server/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            var port = 54678;
+
+            EndPoint listenerEP = new IPEndPoint(ip, port);
+            listener.Bind(listenerEP);
+
+            var buffer = new byte[ushort.MaxValue - 30];
+
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            int len = 0;
+            var msg = "";
+
+            while (true)
+            {
+
+                len = listener.ReceiveFrom(buffer, ref remoteEP);
+
+                msg = Encoding.Default.GetString(buffer, 0, len);
+
+                if (msg == "SS")
+                {
+                    Bitmap memoryImage;
+                    memoryImage = new Bitmap(500, 450); //Shekil
+                    Size s = new Size(memoryImage.Width, memoryImage.Height);
+                    Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+                    memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+                    ImageConverter converter = new ImageConverter();
+                    var bytes = (byte[])converter.ConvertTo(memoryImage, typeof(byte[]));
+
+
+
+                    using var client = new Socket(
+                                     AddressFamily.InterNetwork,
+                                     SocketType.Dgram,
+                                     ProtocolType.Udp);
+
+                    var ipc = IPAddress.Parse("127.0.0.1");
+                    var portc = 27121;
+
+                    
+                    EndPoint remoteEPc = new IPEndPoint(ipc, portc);
+                    client.SendTo(bytes, remoteEPc);
+                }
+
+            }
+        }
+    }
+}
